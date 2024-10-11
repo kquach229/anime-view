080db74c19a3941637417ca3748502fb8c5ecf68
kquach229 <kquach229>
2024-10-10 20:51:08 -0400

    save

--- components/CommentsList.tsx
+++ components/CommentsList.tsx
@@ -4,7 +4,6 @@ const CommentsList = async ({ id }: { id: string }) => {
   const comments = await fetch(`${BASE_URL}/api/comments/${id}`);
   const data = await comments.json();
 
-  console.log('data:::::::::::', data);
   return <div></div>;
 };
 
